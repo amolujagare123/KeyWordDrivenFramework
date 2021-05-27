1f1a9dbef1bde7d5e1d31e2e81bd634ba341d32a
PC <amolujagare@gmail.com>
2021-05-27 20:27:47 +0530

    code updated

--- testsrc/com/regression/login/LoginTest1.java
+++ testsrc/com/regression/login/LoginTest1.java
@@ -1,6 +1,6 @@
 package com.regression.login;
 
-import com.invoiceplane.keyword.Engine.KeyWordEngine830;
+import com.invoiceplane.keyword.Engine.KeyWordEngine730;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -8,21 +8,9 @@ import java.io.IOException;
 public class LoginTest1 {
 
 
-
-   /* KyWordEngineExpaning engine ;
-    @Test
-    public void allLoginPageTest() throws IOException {
-
-        engine = new KyWordEngineExpaning();
-        engine.startExecution("login");
-    }
-*/
-
-
-
     @Test
     public void LoginpageTest() throws IOException {
-        KeyWordEngine830 engine = new KeyWordEngine830();
+        KeyWordEngine730 engine = new KeyWordEngine730();
         engine.startExecution("login");
     }
 }
